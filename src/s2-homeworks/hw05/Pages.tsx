0d408bb0502943964173e107e8d4ca4c6997d737
LexaFollowYourDream <aleksejkundenko10@gmail.com>
2023-02-25 13:11:54 +0300

    feat:hw6 is done

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
 import JuniorPlus from './pages/JuniorPlus'
+
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
@@ -19,16 +20,16 @@ function Pages() {
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                 <Route path={'/'} element={<Navigate to={'/pre-junior'}/>}/>
+                <Route path={'/'} element={<Navigate to={'/pre-junior'}/>}/>
 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
                 <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
                 <Route path={PATH.JUNIOR} element={<Junior/>}/>
-                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>} />
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
                 <Route path={'/*'} element={<Error404/>}/>
-                 <Route />
+                <Route/>
             </Routes>
         </div>
     )
